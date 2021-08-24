8d84dd6c7d7dfe7881afb7b0fd4e378533d33882
rizwanalvi <rizwan@aptechsite.net>
2021-08-24 14:27:28 +0100

    Create assignment.js

---
+++ Day15-Declear and Use a Function/assignment.js
@@ -0,0 +1,21 @@
+function AreaOfCircle(radius){
+const pi = 3.142;
+result = 2 * pi * radius**2;
+return result;
+}
+
+function ShowAlert(){
+    alert('Show');
+
+  }
+  function Greet(msg){
+    alert(`Welcome ${msg}`);
+
+  }    
+  function Addtion(a,b)
+{
+
+    let result = parseInt(a)+parseInt(b);
+     return result;
+
+} 
